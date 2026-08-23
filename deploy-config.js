2a606f6af5d1083cabe27273cfe9ca889baa5278
dusunim <dusunim@dusunimui-Macmini.local>
2026-08-24 04:44:29 +0900

    Update Cloudflare Quick Tunnel URL

--- deploy-config.js
+++ deploy-config.js
@@ -1,4 +1,4 @@
 window.MUSIC_CAFE_CONFIG = {
   // Leave blank for file:// playback. A Cloudflare Tunnel URL can be set here.
-  mediaBaseUrl: 'https://driver-chairs-canyon-tonight.trycloudflare.com',
+  mediaBaseUrl: 'https://skating-hearings-cho-hash.trycloudflare.com',
 };
