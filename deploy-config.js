e1e4e485f8eafa82e0270d1be1cb5cefe7682045
Music Cafe Deploy <music-cafe-deploy@users.noreply.github.com>
2026-09-18 21:28:05 +0900

    Update Cloudflare Quick Tunnel URL

--- deploy-config.js
+++ deploy-config.js
@@ -1,4 +1,4 @@
 window.MUSIC_CAFE_CONFIG = {
   // Leave blank for file:// playback. A Cloudflare Tunnel URL can be set here.
-  mediaBaseUrl: 'https://bikes-poem-naturals-updated.trycloudflare.com',
+  mediaBaseUrl: 'https://isa-wonderful-bandwidth-grill.trycloudflare.com',
 };
