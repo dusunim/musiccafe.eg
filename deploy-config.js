f186bdbe8b8b0ce44e4574dec4735227735cb1be
Music Cafe Deploy <music-cafe-deploy@users.noreply.github.com>
2026-09-18 21:29:18 +0900

    Update Cloudflare Quick Tunnel URL

--- deploy-config.js
+++ deploy-config.js
@@ -1,4 +1,4 @@
 window.MUSIC_CAFE_CONFIG = {
   // Leave blank for file:// playback. A Cloudflare Tunnel URL can be set here.
-  mediaBaseUrl: 'https://isa-wonderful-bandwidth-grill.trycloudflare.com',
+  mediaBaseUrl: 'https://sleeping-olympus-thickness-impressed.trycloudflare.com',
 };
